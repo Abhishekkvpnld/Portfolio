247b7c71d1f0e779784147ac4525f3f3524d3324
Abhishekkv <abhishekkvpnld@gmail.com>
2024-05-16 15:41:54 +0530

    video-call-app added

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -8,108 +8,117 @@ import Work8 from "../../assets/work8.jpg";
 import Work7 from "../../assets/work7.png";
 import Work9 from "../../assets/Work9.png";
 import Work10 from "../../assets/work10.jpg";
+import Work11 from "../../assets/work11.jpg"
 
 
 export const ProjectsData = [
-{
-        id:1,
-    image:Work1,
-    title:'Shopping Cart',
-    category : 'web',
-    link:'https://shopping-cart-q726.onrender.com',
-    code:'https://github.com/Abhishekkvpnld/shoppingcart'
+    {
+        id: 1,
+        image: Work1,
+        title: 'Shopping Cart',
+        category: 'web',
+        link: 'https://shopping-cart-q726.onrender.com',
+        code: 'https://github.com/Abhishekkvpnld/shoppingcart'
 
-},
-{
-    id:2,
-image:Work2,
-title:'Netflix Clone',
-category : 'React',
-link:'https://abhishekkvpnld.github.io/netflix-host/',
-code:'https://github.com/Abhishekkvpnld/Netflix-clone   '
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Netflix Clone',
+        category: 'React',
+        link: 'https://abhishekkvpnld.github.io/netflix-host/',
+        code: 'https://github.com/Abhishekkvpnld/Netflix-clone   '
 
-},
-{
-    id:3,
-image:Work3,
-title:'OLX Clone',
-category : 'React',
-link:'https://abhishekkvpnld.github.io/olx-host/',
-code:'https://github.com/Abhishekkvpnld/olx_clone'
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'OLX Clone',
+        category: 'React',
+        link: 'https://abhishekkvpnld.github.io/olx-host/',
+        code: 'https://github.com/Abhishekkvpnld/olx_clone'
 
-},
-{
-    id:4,
-image:Work4,
-title:'My Portfolio',
-category : 'React',
-link:'https://abhishekkvpnld.github.io/Portfolio/',
-code:'https://github.com/Abhishekkvpnld/My-Portfolio'
-},
-{
-    id:5,
-image:Work5,
-title:'Calculator',
-category : 'static',
-link:' https://abhishekkvpnld.github.io/Calculator/',
-code:'https://github.com/Abhishekkvpnld/Calculator'
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'My Portfolio',
+        category: 'React',
+        link: 'https://abhishekkvpnld.github.io/Portfolio/',
+        code: 'https://github.com/Abhishekkvpnld/My-Portfolio'
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Calculator',
+        category: 'static',
+        link: ' https://abhishekkvpnld.github.io/Calculator/',
+        code: 'https://github.com/Abhishekkvpnld/Calculator'
 
-},
-{
-    id:6,
-image:Work6,
-title:'Chat-app',
-category : 'web',
-link:'https://chat-app-chatme.netlify.app/',
-code:'https://github.com/Abhishekkvpnld/Chat-App'
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: 'Chat-app',
+        category: 'web',
+        link: 'https://chat-app-chatme.netlify.app/',
+        code: 'https://github.com/Abhishekkvpnld/Chat-App'
 
-},
-{
-    id:7,
-image:Work7,
-title:'Authentication-App',
-category : 'web',
-link:'https://authentication-app2.netlify.app/',
-code:'https://github.com/Abhishekkvpnld/Authentication-App.git'
-},
-{
-    id:8,
-image:Work8,
-title:'Social-Media-App',
-category : 'web',
-link:'https://socio-burst.netlify.app/',
-code:'https://github.com/Abhishekkvpnld/Social-Media-App.git'
-},
-{
-    id:9,
-image:Work9,
-title:'Advanced-Chat-App',
-category : 'web',
-link:'https://chat-app-advanced.vercel.app/',
-code:'https://github.com/Abhishekkvpnld/Chat-App-Advanced.git'
-},
-{
-    id:10,
-image:Work10,
-title:'Tic-Tac-Toe-Game',
-category : 'web',
-link:'https://tic-tac-toe-game-two-alpha.vercel.app/',
-code:'https://github.com/Abhishekkvpnld/Tic-Tac-Toe-Game.git'
-},
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: 'Authentication-App',
+        category: 'web',
+        link: 'https://authentication-app2.netlify.app/',
+        code: 'https://github.com/Abhishekkvpnld/Authentication-App.git'
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: 'Social-Media-App',
+        category: 'web',
+        link: 'https://socio-burst.netlify.app/',
+        code: 'https://github.com/Abhishekkvpnld/Social-Media-App.git'
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: 'Advanced-Chat-App',
+        category: 'web',
+        link: 'https://chat-app-advanced.vercel.app/',
+        code: 'https://github.com/Abhishekkvpnld/Chat-App-Advanced.git'
+    },
+    {
+        id: 10,
+        image: Work10,
+        title: 'Tic-Tac-Toe-Game',
+        category: 'web',
+        link: 'https://tic-tac-toe-game-two-alpha.vercel.app/',
+        code: 'https://github.com/Abhishekkvpnld/Tic-Tac-Toe-Game.git'
+    },
+    {
+        id: 11,
+        image: Work11,
+        title: 'VideoCall-app',
+        category: 'web',
+        link: 'https://video-call-app-chi.vercel.app/',
+        code: 'https://github.com/Abhishekkvpnld/Video-Call-App.git'
+    },
 ]
 
 export const projectNav = [
     {
-        name:'all'
+        name: 'all'
     },
     {
-        name:'web'
+        name: 'web'
     },
     {
-        name:"React"
+        name: "React"
     },
     {
-        name:"static"
+        name: "static"
     }
 
 ]
